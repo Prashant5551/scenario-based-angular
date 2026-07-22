aa52cdeb0c71a5911167fc9969684b81d5e738b4
Prashant5551 <prashkbk@gmail.com>
2026-07-22 17:52:19 +0530

    global constant code added

---
+++ scenario_based_angular/src/app/core/constant/Global.constant.ts
@@ -0,0 +1,22 @@
+export const GlobalConstant = {
+    API_END_POINT_CONTROLLER: {
+        EMPLOYEE: 'EmployeeApp'
+    },
+    API_END_POINT_METHOD: {
+        EMPLOYEE: {
+            GET_ALL_EMPLOYEE: 'GetEmployees',
+            CREATE_NEW: 'CreateEmployee'
+        },
+        DEPARTMENT: {
+            GET_ALL_DEPT: 'GetDepartments',
+            CREATE_NEW_DEPT: 'addnewdept'
+        }
+    },
+    VALIDATION_MESSAGE: {
+        REQUIRED: 'this is required'
+    },
+    REG_EXP: {
+        EMAIL: '',
+        PAN: ''
+    }
+}
--- scenario_based_angular/src/app/core/service/employee/employee.ts
+++ scenario_based_angular/src/app/core/service/employee/employee.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Service } from '@angular/core';
 import { environment } from '../../../../environments/environment.development';
+import { GlobalConstant } from '../../constant/Global.constant';
 
 @Service()
 export class Employee {
@@ -9,7 +10,8 @@ export class Employee {
     http = inject(HttpClient);
 
     getAllEmployee() {
-        return this.http.get(environment.API_URL + "EmployeeApp/GetEmployees");
+        return this.http.get(environment.API_URL + GlobalConstant.API_END_POINT_CONTROLLER.EMPLOYEE + '/'
+             +GlobalConstant.API_END_POINT_METHOD.EMPLOYEE.GET_ALL_EMPLOYEE);
     }
 
     createNewEmplyee(obj: any) {
