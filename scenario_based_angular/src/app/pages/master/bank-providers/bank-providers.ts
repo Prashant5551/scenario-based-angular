c494ef400d21555830f6b925847888b514125444
Prashant5551 <prashkbk@gmail.com>
2026-07-22 14:01:22 +0530

    Angular architecture code

---
+++ scenario_based_angular/src/app/pages/master/bank-providers/bank-providers.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-providers',
+  imports: [],
+  templateUrl: './bank-providers.html',
+  styleUrl: './bank-providers.css',
+})
+export class BankProviders {}
---
+++ scenario_based_angular/test.js
@@ -0,0 +1,8 @@
+let email ='pk@mail.com';
+var name = 'prashant';
+const city = 'pune';
+let state;
+
+// alert('hello');
+console.table([email, name, city,state]);
+
